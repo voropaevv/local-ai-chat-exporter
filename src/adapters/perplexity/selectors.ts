801ebea7c8962ff8a209a27273f9ef35eaf76f47
Vlad Voropaev <vladislavvoropaev2001@gmail.com>
2026-09-11 14:34:07 +0400

    fix(perplexity): support current message groups

--- src/adapters/perplexity/selectors.ts
+++ src/adapters/perplexity/selectors.ts
@@ -1,6 +1,6 @@
 export const perplexitySelectors = {
   message:
-    "[data-testid='query-text'], [data-test-id='query-text'], [data-testid='query-content'], [data-testid='thread-question'], [class~='group/query'], [data-testid='answer'], [data-test-id='answer'], [data-testid='answer-content'], [data-testid='thread-answer'], main [id^='markdown-content-'] .prose[data-renderer='lm']",
+    "[data-testid='query-text'], [data-test-id='query-text'], [data-testid='query-content'], [data-testid='thread-question'], [class~='group/query'], [class~='group/user-bubble'], [data-testid='answer'], [data-test-id='answer'], [data-testid='answer-content'], [data-testid='thread-answer'], [class~='group/final-text'], main [id^='markdown-content-'] .prose[data-renderer='lm']",
   content:
     "article, [data-message-content], [data-testid='query-content'], [data-testid='answer-content'], .prose"
 } as const;
